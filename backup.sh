b0da0567ada8a5ae7e48a71358a3ddd8d42cb7f7
Fabian Bucher <git@fabianbucher.de>
2025-01-16 20:14:34 +0100

    backup: update script

--- backup.sh
+++ backup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# exit script on error
+set -exu -o pipefail
+# go to to directory of this script
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $SCRIPT_DIR
 # Import environment from .env file
@@ -7,14 +10,14 @@ export $(egrep -v '^#' .env | xargs)
 IFS=';' read -ra APPS <<< "$APPS_TO_BACKUP"
 # Write email subject
 echo "Subject: $BACKUP_NAME Backup Report"
-DRC_PART_START="docker run --rm -v $HOME/.ssh:/root/.ssh:ro -v $SCRIPT_DIR/borg.yml:/etc/borgmatic/config.yaml:ro -v /etc/localtime:/etc/localtime:ro"
+DRC_PART_START="docker run --rm -v $SCRIPT_DIR/backups:/backup_repos -v $SCRIPT_DIR/borg.yml:/etc/borgmatic/config.yaml:ro -v /etc/localtime:/etc/localtime:ro"
 # Run backup for each app
 for APP in "${APPS[@]}"
 do
   start=`date +%s`
   echo $APP" backup..."
   cd $SCRIPT_DIR/$APP
-  DRC_PART_CUSTOM="monachus/borgmatic borgmatic --override location.repositories=[$BORG_REPO_HOST:$BORG_REPO_DIR/$APP.borg] storage.encryption_passphrase=$BORG_KEY"
+  DRC_PART_CUSTOM="monachus/borgmatic borgmatic --override location.repositories=[/backup_repos/$APP.borg] storage.encryption_passphrase=$BORG_KEY"
   # Initialize repo
   BACKUP_INIT="$DRC_PART_START $DRC_PART_CUSTOM init --encryption repokey"
   eval $BACKUP_INIT
@@ -22,7 +25,7 @@ do
   BACKUP_PRUNE="$DRC_PART_START $DRC_PART_CUSTOM prune"
   eval $BACKUP_PRUNE
   # Stop containers
-  docker-compose stop && echo "  Stopped Containers."
+  docker compose stop && echo "  Stopped Containers."
   # Get volume names
   APP_VOLUMES=$(docker volume ls -q -f "name=$APP")
   # Convert string to array
@@ -31,24 +34,26 @@ do
   BACKUP_VOLUMES=""
   for APP_VOLUME in "${APP_VOLUMES_ARRAY[@]}"
   do
+    echo $APP_VOLUME 
     BACKUP_VOLUMES="$BACKUP_VOLUMES -v $APP_VOLUME:/backup_data/$APP_VOLUME:ro"
   done
   BACKUP_CREATE="$DRC_PART_START $BACKUP_VOLUMES $DRC_PART_CUSTOM create"
   eval $BACKUP_CREATE
   # Start containers
-  docker-compose start && echo "  Started Containers."
+  docker compose start && echo "  Started Containers."
   # Finish mesuring time and display
   end=`date +%s`
   echo "  "$(date -d@$((end-start)) -u +%H:%M:%S)
 done
 echo ""
+docker run --rm -v $SCRIPT_DIR/backups:/backup_repos monachus/borgmatic chown -R 1000 /backup_repos
 # Execute rclone
 echo "---"
 start=`date +%s`
-ssh $BORG_REPO_HOST "rclone sync $BORG_REPO_DIR $RCLONE_DESTINATION --transfers 32"
+rclone sync $SCRIPT_DIR/backups $RCLONE_DESTINATION --transfers 32
 end=`date +%s`
 echo "rclone... "$(date -d@$((end-start)) -u +%H:%M:%S)
 echo ""
 # Display repo sizes
 echo "---"
-echo $(ssh $BORG_REPO_HOST "cd $BORG_REPO_DIR && du -sh *")
+cd $SCRIPT_DIR/backups && du -sh *
